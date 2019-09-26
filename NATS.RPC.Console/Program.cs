fc6d0593d472a5aeac91b3e22eacca942a874d14
Алексей Володин <nerevaider@gmail.com>
2019-09-26 11:08:41 +0300

    Add project files.

---
+++ NATS.RPC.Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using NATS.Client;
+
+namespace NATS.RPC.Console
+{
+    public interface ITest
+    {
+        string Echo(string msg);
+        void Rpc(string msg, int id);
+    }
+
+    internal class Test : ITest
+    {
+        public string Echo(string msg)
+        {
+            System.Console.WriteLine($"Echo: {msg}");
+            return msg;
+        }
+
+        public void Rpc(string msg, int id)
+        {
+            System.Console.WriteLine($"Rpc: {msg} {id}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionFactory = new ConnectionFactory();
+            var url = "nats://localhost:4222";
+            var serviceUid = "Test";
+            //var serviceUid1 = "Test1";
+
+            var serviceFactory = new ServiceFactory(connectionFactory, url);
+            var test = new Test();
+
+            serviceFactory.Create<ITest, Test>(test, serviceUid);
+            //serviceFactory.Create<ITest, Test>(test, serviceUid1);
+
+            var proxyFactory = new ProxyFactory(connectionFactory, url);
+            var proxy = proxyFactory.Create<ITest>(serviceUid);
+            //var proxy1 = proxyFactory.Create<ITest>(serviceUid1);
+
+            var response = proxy.Echo("Hello World!");
+
+            System.Console.WriteLine($"Echo response: {response}");
+
+            proxy.Rpc("RPC", 100);
+
+            //response = proxy1.Echo("!@#");
+
+            //System.Console.WriteLine($"Echo response: {response}");
+
+        }
+    }
+}
---
+++ NATS.RPC/ProxyFactory.cs
@@ -0,0 +1,68 @@
+﻿using Castle.DynamicProxy;
+using NATS.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System;
+
+namespace NATS.RPC
+{
+    public class ProxyFactory
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private readonly ProxyGenerator _proxyGenerator;
+
+        private readonly string _url;
+
+        public ProxyFactory(ConnectionFactory connectionFactory, string url)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+            _url = url ?? throw new ArgumentNullException(nameof(url));
+
+            _proxyGenerator = new ProxyGenerator();
+        }
+
+        public T Create<T>(string serviceUid)
+            where T : class
+        {
+            var connection = _connectionFactory.CreateConnection(_url);
+            var interceptor = new NATSInterceptor(connection, serviceUid ?? throw new ArgumentNullException(nameof(serviceUid)));
+            return _proxyGenerator.CreateInterfaceProxyWithoutTarget<T>(interceptor);
+        }
+    }
+
+    internal class NATSInterceptor : IInterceptor
+    {
+        private readonly IConnection _connection;
+        private readonly string _serviceUid;
+
+        public NATSInterceptor(IConnection connection, string serviceUid)
+        {
+            _connection = connection;
+            _serviceUid = serviceUid;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            try
+            {
+                var subject = invocation.Method.Name;
+                var json = JsonConvert.SerializeObject(invocation.Arguments);
+                var bytes = Encoding.UTF8.GetBytes(json);
+
+                var response = _connection.Request($"{_serviceUid}.{subject}", bytes);
+
+                if (invocation.Method.ReturnType == typeof(void))
+                    return;
+
+                json = Encoding.UTF8.GetString(response.Data);
+                var result = JsonConvert.DeserializeObject(json, invocation.Method.ReturnType);
+
+                invocation.ReturnValue = result;
+            }
+            catch
+            {
+                invocation.ReturnValue = null;
+            }
+        }
+    }
+}
